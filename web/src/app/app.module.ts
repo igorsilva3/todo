bb44086723ec4b013a0b4d3b1789853a996be6a1
Igor Silva <igorbr2018@gmail.com>
2022-01-27 21:01:28 -0300

    Created tasks list component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule,
