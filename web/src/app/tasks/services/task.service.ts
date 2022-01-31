6ae804d2253ead78c2fdd294febfc33eeed09554
Igor Silva <igorbr2018@gmail.com>
2022-01-31 18:31:24 -0300

    Created and added create tasks service

--- web/src/app/tasks/services/task.service.ts
+++ web/src/app/tasks/services/task.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+
 import { GetTasksService } from './get-tasks.service';
 import { DeleteTaskService } from './delete-task.service';
 import { UpdateTaskService } from './update-task.service';
-
+import { CreateTaskService } from './create-task.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,6 +12,7 @@ export class TaskService {
   constructor(
     public getTaskService: GetTasksService,
     public deleteTaskService: DeleteTaskService,
-    public updateTaskService: UpdateTaskService
+    public updateTaskService: UpdateTaskService,
+    public createTaskService: CreateTaskService
   ) { }
 }
