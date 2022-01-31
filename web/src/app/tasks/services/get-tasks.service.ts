07495e847bddec770a86514e12cf768de61debc8
Igor Silva <igorbr2018@gmail.com>
2022-01-31 13:29:23 -0300

    Updated get tasks service

--- web/src/app/tasks/services/get-tasks.service.ts
+++ web/src/app/tasks/services/get-tasks.service.ts
@@ -4,15 +4,15 @@ import { HttpClient } from '@angular/common/http';
 import { Task } from '../../models/task.model'
 import { Observable } from 'rxjs/internal/Observable';
 
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class GetTasksService {
-  private baseUrl = 'http://localhost:3000'
-
   constructor(private httpClient: HttpClient) { }
+  private apiUrl = environment.apiUrl
 
   public execute(): Observable<Task[]> {
-    return this.httpClient.get<Task[]>(`${this.baseUrl}/tasks/`)
+    return this.httpClient.get<Task[]>(`${this.apiUrl}/tasks/`)
   }
 }
