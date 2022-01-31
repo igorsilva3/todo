033a55fe14e0350bcbb5ffca305f620077e15e64
Igor Silva <igorbr2018@gmail.com>
2022-01-31 11:52:34 -0300

    Render tasks

--- web/src/app/tasks/services/get-tasks.service.ts
+++ web/src/app/tasks/services/get-tasks.service.ts
@@ -8,19 +8,34 @@ import { Task } from '../../models/task.model'
 export class GetTasksService {
   constructor() { }
 
-  public static execute(): Task[] {
+  public execute(): Task[] {
     return [
       {
         id: 1,
         title: 'Task 1',
         description: 'Task 1 description',
-        status: 'done',
+        status: 'open',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         id: 2,
         title: 'Task 2',
+        status: 'open',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 3,
+        title: 'Task 3',
+        description: 'Task 3 description',
+        status: 'done',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 4,
+        title: 'Task 4',
         status: 'open',
         createdAt: new Date(),
         updatedAt: new Date(),
--- web/src/app/tasks/tasks-list/tasks-list.component.ts
+++ web/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GetTasksService } from './../services/get-tasks.service';
+
+import { Task } from './../../models/task.model';
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+  styleUrls: ['./tasks-list.component.css'],
+  providers: [ GetTasksService ]
 })
 export class TasksListComponent implements OnInit {
+  public tasks: Task[]
 
-  constructor() { }
+  constructor(private service: GetTasksService) { }
 
   ngOnInit(): void {
+    this.tasks = this.service.execute()
   }
 
 }
