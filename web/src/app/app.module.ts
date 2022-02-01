8185dea3dceb9c8a1f4cf34ca9eb21345d750d27
Igor Silva <igorbr2018@gmail.com>
2022-02-01 17:05:16 -0300

    Created tasks

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -20,7 +21,8 @@ import { CreateTasksComponent } from './tasks/create-tasks/create-tasks.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/tasks/create-tasks/create-tasks.component.ts
+++ web/src/app/tasks/create-tasks/create-tasks.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { TaskService } from './../services/task.service'
+
+type CreateTaskDTO = {
+  title: string
+  description?: string
+  status: 'open' | 'done'
+}
 
 @Component({
   selector: 'app-create-tasks',
   templateUrl: './create-tasks.component.html',
-  styleUrls: ['./create-tasks.component.css']
+  styleUrls: ['./create-tasks.component.css'],
+  providers: [TaskService],
 })
 export class CreateTasksComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public dataForm: CreateTaskDTO = {
+    title: '',
+    description: '',
+    status: 'open',
   }
 
+  constructor(private service: TaskService) {}
+
+  ngOnInit(): void {}
+
+  public createTask(): void {
+    this.service.createTaskService
+      .execute(this.dataForm)
+      .subscribe(() => {})
+
+    // Clear form
+    this.dataForm = {
+      ...this.dataForm,
+      title: '',
+      description: '',
+    }
+  }
 }
