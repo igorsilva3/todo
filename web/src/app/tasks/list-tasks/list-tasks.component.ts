62118a7de998d5ab03910453cad8ef708c1d2294
Igor Silva <igorbr2018@gmail.com>
2022-02-06 15:41:57 -0300

    Added check task component

--- web/src/app/tasks/list-tasks/list-tasks.component.ts
+++ web/src/app/tasks/list-tasks/list-tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import { TaskService } from '../services/task.service';
 
@@ -9,20 +9,15 @@ import { Task } from '../../models/task.model';
   styleUrls: ['./list-tasks.component.css'],
   providers: [ TaskService ]
 })
-export class ListTasksComponent implements OnInit {
+export class ListTasksComponent implements OnChanges {
   @Input()
   public tasks: Task[]
 
   constructor(private service: TaskService) { }
 
-  ngOnInit(): void {
-  }
-
-  public updateTask({ id, data }: { id: number, data: Partial<Task> }) {
-    this.service.updateTaskService.execute({ id, data }).subscribe(task => {
-      const index = this.tasks.findIndex(t => t.id === id)
-      this.tasks[index] = task
+  ngOnChanges(changes: SimpleChanges): void {
+    this.service.getTasksService.execute().subscribe(tasks => {
+      this.tasks = tasks
     })
   }
-
 }
