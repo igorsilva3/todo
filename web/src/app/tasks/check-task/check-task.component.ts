179f18508fb1af322ce393ed95ff0f8646c81e04
Igor Silva <igorbr2018@gmail.com>
2022-02-06 15:41:32 -0300

    Added styles and execute check task

--- web/src/app/tasks/check-task/check-task.component.ts
+++ web/src/app/tasks/check-task/check-task.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
 
+import { TaskService } from './../services/task.service';
+
+import { Task } from './../../models/task.model'
 @Component({
   selector: 'app-check-task',
   templateUrl: './check-task.component.html',
-  styleUrls: ['./check-task.component.css']
+  styleUrls: ['./check-task.component.css'],
+  providers: [TaskService]
 })
 export class CheckTaskComponent implements OnInit {
+  @Input() task: Task
 
-  constructor() { }
+  constructor(private service: TaskService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public checkTask() {
+    this.service.updateTaskService.execute({
+      id: this.task.id,
+      data: {
+        ...this.task,
+        status: 'done'
+      }
+    }).subscribe(() => {})
   }
 
+  public unCheckTask() {
+    this.service.updateTaskService.execute({
+      id: this.task.id,
+      data: {
+        ...this.task,
+        status: 'open'
+      }
+    }).subscribe(() => {})
+  }
 }
