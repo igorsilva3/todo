2a3fc6c177fa163b80411559c6cf2bc250c5cec0
Igor Silva <igorbr2018@gmail.com>
2022-02-10 19:36:05 -0300

    Generated update task component

--- web/src/app/tasks/tasks.module.ts
+++ web/src/app/tasks/tasks.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ListTasksComponent } from './list-tasks/list-tasks.component';
 import { CreateTaskComponent } from './create-task/create-task.component';
 import { DeleteTaskComponent } from './delete-task/delete-task.component';
 import { CheckTaskComponent } from './check-task/check-task.component';
+import { UpdateTaskComponent } from './update-task/update-task.component';
 
 @NgModule({
   declarations: [
     ListTasksComponent,
     CreateTaskComponent,
     DeleteTaskComponent,
-    CheckTaskComponent
+    CheckTaskComponent,
+    UpdateTaskComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
+    NgbModalModule
   ],
   exports: [
     CreateTaskComponent,
