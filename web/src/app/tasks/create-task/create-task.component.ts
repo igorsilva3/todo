a5e8afa1e0452547b5854f28a3c0bb2892f9b2b8
Igor Silva <igorbr2018@gmail.com>
2022-03-03 20:46:34 -0300

    Added tasks observable for notify new task

--- web/src/app/tasks/create-task/create-task.component.ts
+++ web/src/app/tasks/create-task/create-task.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core'
-import { TaskService } from '../services/task.service'
+import { Component, OnInit } from '@angular/core'
 
 import { Task } from '../../models/task.model'
+
+import { TaskService } from '../services/task.service'
+import { TasksObservableService } from './../services/tasks-observable.service';
 
 type CreateTaskDTO = {
   title: string
@@ -22,14 +24,23 @@ export class CreateTaskComponent implements OnInit {
     status: 'open',
   }
 
-  constructor(private service: TaskService) {}
+  constructor(private service: TaskService, private tasksObservableService: TasksObservableService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   public createTask(): void {
+    let tasks: Task[] = []
+
+    this.service.getTasksService.execute().subscribe((_tasks) => {
+      tasks = _tasks
+    })
+
     this.service.createTaskService.execute(this.dataForm).subscribe(() => {
       this.clearForm()
     })
+
+    this.tasksObservableService.notifyTasks(tasks)
   }
 
   private clearForm() {
