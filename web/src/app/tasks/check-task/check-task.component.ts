7d9a96b77f32bcf6a7079633ac42e82379fd917a
Igor Silva <igorbr2018@gmail.com>
2022-03-03 21:21:14 -0300

    Added tasks observable for check and uncheck task

--- web/src/app/tasks/check-task/check-task.component.ts
+++ web/src/app/tasks/check-task/check-task.component.ts
@@ -1,38 +1,72 @@
+import { TasksObservableService } from './../services/tasks-observable.service'
 import { Component, Input, OnInit } from '@angular/core'
 
-import { TaskService } from './../services/task.service';
+import { TaskService } from './../services/task.service'
 
 import { Task } from './../../models/task.model'
 @Component({
   selector: 'app-check-task',
   templateUrl: './check-task.component.html',
   styleUrls: ['./check-task.component.css'],
-  providers: [TaskService]
+  providers: [TaskService],
 })
 export class CheckTaskComponent implements OnInit {
   @Input() task: Task
 
-  constructor(private service: TaskService) {}
+  private tasks: Task[] = []
 
-  ngOnInit(): void {}
+  constructor(
+    private service: TaskService,
+    private tasksObservableService: TasksObservableService,
+  ) {}
+
+  ngOnInit(): void {
+    this.tasksObservableService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks
+    })
+  }
 
   public checkTask() {
-    this.service.updateTaskService.execute({
-      id: this.task.id,
-      data: {
-        ...this.task,
-        status: 'done'
-      }
-    }).subscribe(() => {})
+    this.service.updateTaskService
+      .execute({
+        id: this.task.id,
+        data: {
+          ...this.task,
+          status: 'done',
+        },
+      })
+      .subscribe((_task) => {
+        this.tasks = this.tasks.map((task) => {
+          if (task.id !== _task.id) {
+            return task
+          }
+
+          return _task
+        })
+      })
+
+    this.tasksObservableService.notifyTasks(this.tasks)
   }
 
   public unCheckTask() {
-    this.service.updateTaskService.execute({
-      id: this.task.id,
-      data: {
-        ...this.task,
-        status: 'open'
-      }
-    }).subscribe(() => {})
+    this.service.updateTaskService
+      .execute({
+        id: this.task.id,
+        data: {
+          ...this.task,
+          status: 'open',
+        },
+      })
+      .subscribe((_task) => {
+        this.tasks = this.tasks.map((task) => {
+          if (task.id !== _task.id) {
+            return task
+          }
+
+          return _task
+        })
+      })
+
+    this.tasksObservableService.notifyTasks(this.tasks)
   }
 }
