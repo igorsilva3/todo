ba672e1c03300067839632fde3bf3a8c1560e6c4
Igor Silva <igorbr2018@gmail.com>
2022-03-03 21:10:26 -0300

    Added tasks observable for remove task

--- web/src/app/tasks/delete-task/delete-task.component.ts
+++ web/src/app/tasks/delete-task/delete-task.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core'
 
-import { TaskService } from '../services/task.service'
-
 import { Task } from '../../models/task.model'
 
+import { TaskService } from '../services/task.service'
+import { TasksObservableService } from '../services/tasks-observable.service'
 @Component({
   selector: 'app-delete-task',
   templateUrl: './delete-task.component.html',
@@ -12,13 +12,23 @@ import { Task } from '../../models/task.model'
 export class DeleteTaskComponent implements OnInit {
   @Input() taskId: number
 
-  constructor(private service: TaskService) {}
+  private tasks: Task[] = []
 
-  ngOnInit(): void {}
+  constructor(private service: TaskService, private tasksObservableService: TasksObservableService) {}
+
+  ngOnInit(): void {
+    this.tasksObservableService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks
+    })
+  }
 
   public deleteTask() {
     this.service.deleteTaskService
       .execute({ id: this.taskId })
-      .subscribe()
+      .subscribe(() => {
+        this.tasks = this.tasks.filter((task) => task.id !== this.taskId)
+      })
+
+    this.tasksObservableService.notifyTasks(this.tasks)
   }
 }
