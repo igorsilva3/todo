6f6c5553186cf3b2a6c6ed292dbb6176f32e618b
Igor Silva <igorbr2018@gmail.com>
2022-03-27 13:30:03 -0300

    Updated app module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,23 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { TasksModule } from './tasks/tasks.module';
+import { AccountModule } from './account/account.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    TasksModule
+    TasksModule,
+    AccountModule
   ],
   providers: [],
   bootstrap: [AppComponent]
