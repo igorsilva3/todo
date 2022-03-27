6518ba8b0392014c081f532782d0610da823ffd1
Igor Silva <igorbr2018@gmail.com>
2022-03-27 13:26:19 -0300

    Moved login component for account module

--- web/src/app/account/login/login.component.ts
+++ web/src/app/account/login/login.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public dataForm = {
+    email: '',
+    password: ''
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+
   }
 
 }
