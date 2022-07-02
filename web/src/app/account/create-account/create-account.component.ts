aca22023481889cffc2053c8e9054b238b8c9088
Igor Silva <igorbr2018@gmail.com>
2022-07-02 19:27:19 -0300

    Updated signup service

--- web/src/app/account/create-account/create-account.component.ts
+++ web/src/app/account/create-account/create-account.component.ts
@@ -1,27 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+
+import { SignupService } from './../services/signup.service'
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrls: ['./create-account.component.css']
+  styleUrls: ['./create-account.component.css'],
 })
 export class CreateAccountComponent implements OnInit {
   public accountCreateForm = {
     firstName: '',
     lastName: '',
     email: '',
-    password: ''
+    password: '',
   }
 
   public errorMessage = ''
 
-  constructor() { }
+  constructor(private signupService: SignupService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public onSubmit() {
-    console.log(this.accountCreateForm);
+    try {
+      this.signupService.execute(this.accountCreateForm).subscribe(() => {
+        this.router.navigate(['/account/login'])
+      })
+    } catch (error: any) {
+      this.errorMessage = error.message
+    }
   }
-
 }
--- web/src/app/account/services/signup.service.ts
+++ web/src/app/account/services/signup.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
+import { environment } from 'src/environments/environment'
+
+type SignupDTO = {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SignupService {
+  private apiUrl = environment.apiUrl
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  public execute({ firstName, lastName, email, password }: SignupDTO) {
+    if (!firstName || !lastName || !email || !password) {
+      throw new Error('All fields are required')
+    }
+
+    return this.httpClient.post(`${this.apiUrl}/users`, {
+      firstName,
+      lastName,
+      email,
+      password,
+    })
+  }
 }
